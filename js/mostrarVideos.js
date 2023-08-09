1601b604e6cb148ee63676e349d1dbfd0c6d6384
CamilaVerso <camilaverso2@gmail.com>
2023-08-08 22:37:36 -0300

    Incluído o envio de novos videos e a funcionalidade de pesquisa

--- js/mostrarVideos.js
+++ js/mostrarVideos.js
@@ -2,11 +2,10 @@ import { conectaApi } from "./conectaApi.js";
 
 const lista = document.querySelector('[data-lista]');
 
-function constroiCard(titulo, descricao, url, imagem) {
-	const video = document.createElement('li');
+export default function constroiCard(titulo, descricao, url, imagem) {
+	const video = document.createElement("li");
 	video.className = "videos__item";
-	video.innerHTML = `<li class="videos__item">
-	<iframe width="100%" height="72%" src="${url}"
+	video.innerHTML = `<iframe width="100%" height="72%" src="${url}"
                 title="${titulo}" frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowfullscreen></iframe>
@@ -15,7 +14,7 @@ function constroiCard(titulo, descricao, url, imagem) {
                 <h3>${titulo}</h3>
                 <p>${descricao}</p>
             </div>
-			</li>
+			
 	`
 	return video;
 }
